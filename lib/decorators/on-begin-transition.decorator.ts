e91bd91acde79b8b00ec95e0a9a619b6e027c55d
Alex Stansfield <alex.stansfield@gmail.com>
2022-01-22 14:19:07 +0700

    fix: updated dependencies to more recent versions of nestjs

--- lib/decorators/on-begin-transition.decorator.ts
+++ lib/decorators/on-begin-transition.decorator.ts
@@ -2,9 +2,9 @@ import { SetMetadata } from '@nestjs/common';
 import { OnEventMetadata } from '@nestjs/event-emitter';
 
 export const OnBeginTransition = (
-  graphName: string = '*',
-  state: string = '*',
-  async: boolean = false,
+  graphName = '*',
+  state = '*',
+  async = false,
 ): MethodDecorator =>
   SetMetadata('EVENT_LISTENER_METADATA', {
     event: `state-machine.${graphName}.transition.${state}`,
